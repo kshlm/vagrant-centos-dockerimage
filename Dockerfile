b0a83ac3343b8c3675420587e737de04d5e51d5c
Kaushal M <kshlmster@gmail.com>
2015-09-10 13:08:29 +0530

    SystemDed

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM centos:7.1.1503
 MAINTAINER Kaushal M kshlmster@gmail.com
 
+ENV container docker
+
 ADD ./build.sh /
 RUN /build.sh && rm /build.sh
 
-CMD ["/usr/sbin/sshd", "-D"]
+VOLUME ["/sys/fs/cgroup"]
+CMD ["/usr/sbin/init"]
--- build.sh
+++ build.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+yum swap -- remove fakesystemd -- install systemd systemd-libs
 yum install -y shadow-utils sudo openssh-server
 yum clean all
 
+# Setup SystemD
+# Taken from github.com/dockerimages/docker-systemd
+(cd /lib/systemd/system/sysinit.target.wants/; for i in *; do [ $i == systemd-tmpfiles-setup.service ] || rm -f $i; done); \
+rm -f /lib/systemd/system/multi-user.target.wants/*;\
+rm -f /etc/systemd/system/*.wants/*;\
+rm -f /lib/systemd/system/local-fs.target.wants/*; \
+rm -f /lib/systemd/system/sockets.target.wants/*udev*; \
+rm -f /lib/systemd/system/sockets.target.wants/*initctl*; \
+rm -f /lib/systemd/system/basic.target.wants/*;\
+rm -f /lib/systemd/system/anaconda.target.wants/*;
+
+systemctl set-default multi-user.target
+
+# Setup SSH
+
 ssh-keygen -A
+systemctl enable sshd.service
+
+# Setup users
 
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/wheel-nopasswd
 
