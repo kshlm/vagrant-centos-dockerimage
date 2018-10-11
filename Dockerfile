050531b9eff946c24e2206a34108fcc89959c526
Kaushal M <kshlmster@gmail.com>
2018-10-11 16:33:07 +0530

    Update image

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM centos/systemd:latest
+FROM centos:7
 MAINTAINER Kaushal M kshlmster@gmail.com
+
+ENV container docker
 
 ADD ./build.sh /
 RUN /build.sh && rm /build.sh
 
+VOLUME ["/sys/fs/cgroup"]
 CMD ["/usr/sbin/init"]
--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+
 rm -f /usr/lib/tmpfiles.d/systemd-nologin.conf
 
-yum install -y shadow-utils sudo openssh-server openssh-clients
+yum install -y shadow-utils sudo openssh-server openssh-clients libselinux-python
 yum clean all
+
+# Setup SystemD
+# Taken from github.com/dockerimages/docker-systemd
+(cd /lib/systemd/system/sysinit.target.wants/; for i in *; do [ $i == systemd-tmpfiles-setup.service ] || rm -f $i; done); \
+rm -f /etc/systemd/system/*.wants/*;\
+rm -f /lib/systemd/system/local-fs.target.wants/*; \
+rm -f /lib/systemd/system/sockets.target.wants/*udev*; \
+rm -f /lib/systemd/system/sockets.target.wants/*initctl*; \
+rm -f /lib/systemd/system/basic.target.wants/*;\
+rm -f /lib/systemd/system/anaconda.target.wants/*;
+
+systemctl set-default multi-user.target
 
 # Setup SSH
 
